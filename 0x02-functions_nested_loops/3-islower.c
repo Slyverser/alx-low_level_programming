627d1209f3d92a5ab50ffac1c20786936295241f
Slyverser <slyverser@gmail.com>
2023-09-13 16:33:17 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - check for lowercase charater
  * @c: The character to be checked
- * Return ; 1 for lower charater or 0 forn anything else
+ * Return: 1 for lower charater or 0 forn anything else
  */
 
 int _islower(int c)
