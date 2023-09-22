173f8ad88704b80485c75c52a0fe45d6fb3f8dbc
Slyverser <slyverser@gmail.com>
2023-09-22 00:51:55 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,7 +9,7 @@
  * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
-{ 
+{
 	int i;
 	int j;
 
@@ -26,5 +26,5 @@ char *_strncat(char *dest, char *src, int n)
 	j++;
 	}
 	dest[i] = '\0';
-	return (des);
+	return (dest);
 }
