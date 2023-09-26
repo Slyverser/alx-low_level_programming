bee2dd7455ba7fb96d02c68559cf8988ada8f06d
Slyverser <slyverser@gmail.com>
2023-09-26 10:06:40 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _memcpy - a function that copies memory area 
- * @dest: memory where is stored
- * @src: memory where is copied
- * @n: number of bytes
+ * _memcpy - a function that copies memory area
+ * @dest: The destination memory where data is copied
+ * @src: the source memory from which data is copied
+ * @n: The number of bytes to copy.
  *
- * Return : copied memory with n bytes changed
+ * Return : A pointer to the 'dest' memory where the copied data is stored.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
